baca46a634713981f0843c24ea3dc841f825b75c
David Mosher <davidmosher@gmail.com>
2013-06-12 16:46:38 -0600

    task configuration

--- with-lineman/config/application.js
+++ with-lineman/config/application.js
@@ -23,4 +23,53 @@ module.exports = require(process.env['LINEMAN_MAIN']).config.extend('application
   //   }
   // }
 
+  // configure lineman to load additional angular related npm tasks
+  loadNpmTasks: [
+    "grunt-angular-templates",
+    "grunt-ngmin"
+  ],
+
+  // configuration for grunt-angular-templates
+  ngtemplates: {
+    app: { // "app" matches the name of the angular module defined in app.js
+      options: {
+        base: "app/templates"
+      },
+      src: "app/templates/**/*.html",
+      // puts angular templates in a different spot than lineman looks for other templates in order not to conflict with the watch process
+      dest: "generated/angular/template-cache.js"
+    }
+  },
+
+  // configuration for grunt-ngmin, this happens _after_ concat once, which is the ngmin ideal :)
+  ngmin: {
+    js: {
+      src: "<%= files.js.concatenated %>",
+      dest: "<%= files.js.concatenated %>"
+    }
+  },
+
+  // task override configuration
+  prependTasks: {
+    dist: ["ngmin"],         // ngmin should run in dist only
+    common: ["ngtemplates"] // ngtemplates runs in dist and dev
+  },
+
+  // grunt-angular-templates expects that a module already be defined to inject into
+  // this configuration orders the template inclusion _after_ the app level module
+  concat: {
+    js: {
+      src: ["<banner:meta.banner>", "<%= files.js.vendor %>", "<%= files.coffee.generated %>", "<%= files.js.app %>", "<%= files.ngtemplates.dest %>"],
+      separator: ";"
+    }
+  },
+
+  // configures grunt-watch-nospawn to listen for changes to, and recompile angular templates
+  watch: {
+    ngtemplates: {
+      files: "app/templates/**/*.html",
+      tasks: ["ngtemplates", "concat"]
+    }
+  }
+
 });
