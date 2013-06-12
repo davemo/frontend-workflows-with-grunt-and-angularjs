8af670bd442f505ac5dd3761833d8873c14f7849
David Mosher <davidmosher@gmail.com>
2013-06-12 16:45:48 -0600

    directives!

---
+++ with-lineman/app/js/directives/shows_message_when_hovered.js
@@ -0,0 +1,16 @@
+angular.module("app").directive("showsMessageWhenHovered", function() {
+  return {
+    restrict: "A", // A = Attribute, C = CSS Class, E = HTML Element, M = HTML Comment
+    link: function(scope, element, attributes) {
+      var originalMessage = scope.message;
+      element.bind("mouseenter", function() {
+        scope.message = attributes.message;
+        scope.$apply();
+      });
+      element.bind("mouseleave", function() {
+        scope.message = originalMessage;
+        scope.$apply();
+      });
+    }
+  };
+});
